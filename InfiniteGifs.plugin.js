a6ba64e41f146abaacd3611d0f1fcb5ab01736dd
VWilk <wilkvictor07@gmail.com>
2025-04-10 20:09:32 +0100

    Fixing stuff! :D

--- InfiniteGifs.plugin.js
+++ InfiniteGifs.plugin.js
@@ -4,20 +4,31 @@
  * @authorId 363358047784927234
  * @version 1.0.0
  * @description A BetterDiscord plugin to fetch and display GIFs from a GitHub repository.
- * @source https://github.com/VWilk/gifstorage/
+ * @source https://github.com/VWilk/InfiniteGifsBD
  */
 
-let config = {
+const config = {
+    info: {
+        name: "InfiniteGifs",
+        authors: [{
+            name: "VWilk",
+            discord_id: "363358047784927234"
+        }],
+        version: "1.0.0",
+        description: "A BetterDiscord plugin to fetch and display GIFs from a GitHub repository.",
+        github: "https://github.com/VWilk/InfiniteGifsBD",
+    },
+    
     changelog: [
         {
             title: "Version 1.0.0",
             type: "added",
-            items: ["Initial release"],
+            items: ["Initial release"]
         },
         {
-            title: "Bugs Sat On",
+            title: "Bugs Fixed",
             type: "fixed",
-            items: ["Fixed the initial release"],
+            items: ["Fixed the initial release"]
         },
         {
             title: "On-going",
@@ -26,104 +37,93 @@ let config = {
                 "Automatically sync with GitHub?",
                 "Sync between mobile client.",
                 "Different gif profiles.",
-                "Gif search?",
-            ],
-        },
+                "Gif search?"
+            ]
+        }
     ],
-    settings: [
+    defaultConfig: [
         {
             type: "category",
             id: "basic",
-            name: "Setup",
+            name: "Github Setup",
             collapsible: true,
-            shown: true,
+            shown: false,
             settings: [
                 {
-                type: "category",
-                id: "basic",
-                name: "Setup",
-                collapsible: true,
-                shown: true,
-                settings: [
-                    {
-                        type: "text",
-                        id: "userGithubToken",
-                        name: "GitHub API Token",
-                        note: "GitHub API token here!",
-                        value: "",
-                    },
-                    {
-                        type: "text",
-                        id: "userGithubRepositoryName",
-                        name: "Github Repository Name eg: username/githubRepository",
-                        note: "Github Repository Name eg: username/githubRepository",
-                        value: "",
-                    },
-                    {
-                        type: "switch",
-                        id: "GithubOnOff",
-                        name: "GithubSwitch",
-                        note: "Turn on or off functionality of Github.",
-                        value: false,
-                    },
-                ]
-            ],
-        },
-    ],
-    saveSettings() {
-        BdApi.Data.save("InfiniteGifs", "settings", this.settings);
-    },
-    loadSettings() {
-        const loaded = BdApi.Data.load("InfiniteGifs", "settings");
-        if (loaded) {
-            this.settings = loaded;
+                    type: "text",
+                    id: "userGithubToken",
+                    name: "Github API Token",
+                    note: "Github API token here!",
+                    value: ""
+                },
+                {
+                    type: "text",
+                    id: "userGithubRepositoryName",
+                    name: "Github Repository Name",
+                    note: "Format: username/repository",
+                    value: "VWilk/gifstorage"
+                },
+                {
+                    type: "switch",
+                    id: "GithubOnOff",
+                    name: "Enable GitHub Integration",
+                    note: "Turn on or off GitHub functionality",
+                    value: false
+                }
+            ]
         }
-    },
-
-    findSetting(id) {
-        console.log(id)
-        return this.settings[0].settings.find(s => s.id === id);
-    }
+    ]
 };
 
 class GithubAdapter {
-    
+    // Empty adapter class as requested
+    constructor() {}
 }
 
-
-
 class InfiniteGifs {
-    constructor(meta) {
-        this.meta = meta;
-        this.githubRepo = "VWilk/gifstorage";
-        config.loadSettings();
-        this.githubToken = config.findSetting("userGithubToken").value;
+    constructor() {
+        this.settings = this.loadSettings();
+        this.githubAdapter = null;
+    }
+
+    loadSettings() {
+        let saved = BdApi.Data.load("InfiniteGifs", "settings");
+        console.log(saved)
+        return saved || JSON.parse(JSON.stringify(config.defaultConfig));
+    }
+
+    saveSettings() {
+        BdApi.Data.save("InfiniteGifs", "settings", this.settings);
+    }
+
+    findSetting(id) {
+        for (const category of this.settings) {
+            const setting = category.settings.find(s => s.id === id);
+            if (setting) return setting;
+        }
+        return null;
     }
 
     start() {
+        console.log(`${config.info.name} plugin started`);
     }
 
     stop() {
-        console.log("Plugin disabled.");
+        console.log(`${config.info.name} plugin stopped`);
     }
-
 
     getSettingsPanel() {
         return BdApi.UI.buildSettingsPanel({
-            settings: config.settings,
+            settings: this.settings,
             onChange: (settingGroup, settingId, value) => {
-                this.updateUserSettings(settingGroup, settingId, value);
-            },
+                const setting = this.findSetting(settingId);
+                if (setting) {
+                    setting.value = value;
+                    this.saveSettings();
+                }
+            }
         });
     }
-
-    updateUserSettings(settingGroup, settingId, value) {
-        const setting = config.findSetting(settingId);
-        if (setting) setting.value = value;
-        config.saveSettings();
-    }
-
-};
-
+}
 
 module.exports = InfiniteGifs;
